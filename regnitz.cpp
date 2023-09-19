0ffcec1db9e057b9d69c4ed2a64579f29a0d8837
Caroline Collischon <caroline.collischon@fau.de>
2023-09-19 15:29:53 +0200

    License headers

--- regnitz.cpp
+++ regnitz.cpp
@@ -1,3 +1,15 @@
+/*
+ * This file is part of litchi, a lightweight C++ library
+ * for Minkowski analysis
+ * 
+ * Copyright (C) 2021-2023 Caroline Collischon <caroline.collischon@fau.de>
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+
 #include "litchi_eat.hpp"
 
 #include <iostream>
